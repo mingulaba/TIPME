cde2437f41560b8a0886e55481377c2154178b62
Maria Kudashkina <bs.masha2010@gmail.com>
2022-12-05 17:08:47 +0100

    Models and controllers

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,6 @@
 class Restaurant < ApplicationRecord
   belongs_to :user
   has_many :team_members
+  
+  validates :name, presence: true
 end
--- app/models/team_member.rb
+++ app/models/team_member.rb
@@ -2,5 +2,5 @@ class TeamMember < ApplicationRecord
   belongs_to :restaurant
   belongs_to :user
 
-  validates :first_name, :last_name, :introduction, :birth_date, presence: true
+  validates :first_name, :last_name, :birth_date, presence: true
 end
