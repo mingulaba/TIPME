fccaa15f2aa6ad31a651ce219162fdf03be11f0b
Jeremy Buronyi <jeremy.buronyi@gmail.com>
2022-12-06 18:39:26 +0100

    Finished search bar and back end

--- app/controllers/team_members_controller.rb
+++ app/controllers/team_members_controller.rb
@@ -1,2 +1,12 @@
 class TeamMembersController < ApplicationController
+  def index
+    @team_members = TeamMember.order(first_name: :asc)
+
+    @team_members = @team_members.where("first_name ILIKE ?", "%#{params[:query]}%") if params[:query].present?
+
+    respond_to do |format|
+      format.html
+      format.text { render partial: "team_members/list", locals: { team_members: @team_members }, formats: [:html] }
+    end
+  end
 end
