ec22901e1af8e839ccce795045cb38d2ae81841d
Jeremy Buronyi <jeremy.buronyi@gmail.com>
2022-12-08 16:19:15 +0100

    Fixed the route to the team_members index and added a link to the relevant show page

--- app/controllers/team_members_controller.rb
+++ app/controllers/team_members_controller.rb
@@ -1,26 +1,30 @@
 class TeamMembersController < ApplicationController
+  before_action :set_restaurant, only: %i[index show]
 
-  #find a team_member with params
-  #store it
-
-  def show
-    @restaurant = Restaurant.find(params[:restaurant_id])
-    @team_member = TeamMember.find(params[:id])
-    authorize @team_member
-    @table = Table.new
   def index
     @team_members = policy_scope(TeamMember)
 
-    @team_members = @team_members.order(first_name: :asc)
+    @team_members = @restaurant.team_members.order(first_name: :asc)
 
-    @team_members = @team_members.where("first_name ILIKE ?", "%#{params[:query]}%") if params[:query].present?
+    @team_members = @restaurant.team_members.where("first_name ILIKE ?", "%#{params[:query]}%") if params[:query].present?
 
     respond_to do |format|
       format.html
       format.text { render partial: "team_members/list", locals: { team_members: @team_members }, formats: [:html] }
     end
   end
-end
 
+  def show
+    @team_member = TeamMember.find(params[:id])
+    authorize @team_member
+    @table = Table.new
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+end
 
 # restaurant_team_member GET    /restaurants/:restaurant_id/team_members/:id(.:format)                                            team_members#show
