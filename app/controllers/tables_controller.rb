12abc5a18d89b239cf502ccea9297819e93351be
Maria Kudashkina <bs.masha2010@gmail.com>
2022-12-08 16:08:36 +0100

    Show page is done and connected to the confirm page

--- app/controllers/tables_controller.rb
+++ app/controllers/tables_controller.rb
@@ -7,7 +7,7 @@ class TablesController < ApplicationController
       #when we integrate the payment, we shouls be redirect to the pyment_new_path
       #scebario 2 - redirect to the confirmation page
       #NB!
-      redirect_to restaurant_team_member_path(@team_member.restaurant, @team_member)
+      redirect_to payment_confirm_path(@team_member.restaurant, @team_member)
     else
       flash[:alert] = "Something went wrong."
       render :new
