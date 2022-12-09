52dc5d97904c265f608451441fe472229b50fba0
Maria Kudashkina <bs.masha2010@gmail.com>
2022-12-09 12:08:39 +0100

    User story is done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,8 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: %i[home payment_confirm ]
 
   def home
+    # @restaurant = Restaurant.find(id: 1)
   end
 
   def payment_confirm
