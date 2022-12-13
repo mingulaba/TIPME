9fec564a2b9794179e91e4d90ed2ac478e0f24b2
Maria Kudashkina <bs.masha2010@gmail.com>
2022-12-13 22:16:49 +0200

    update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,7 +9,7 @@ class PagesController < ApplicationController
     @user = current_user
     @team_member = TeamMember.find(params[:team_member_id])
   end
-  
+
   def dashboard
     @user = current_user
     @restaurants = Restaurant.where(user: current_user)
